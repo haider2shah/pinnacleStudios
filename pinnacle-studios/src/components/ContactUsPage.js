6d938dc0388894ea36a880d26a45db3ad7257be0
hamza hussain <smhh0@MaeyHamsLaptop>
2023-05-23 20:21:33 -0700

    updated-contact-page

--- pinnacle-studios/src/components/ContactUsPage.js
+++ pinnacle-studios/src/components/ContactUsPage.js
@@ -12,11 +12,33 @@ const ContactUsPage = () => {
                 <div className="ContactUsPage-container">
                     <div className="ContactUsPage-content">
                         <div className="container">
-                        <div class="gradient-bg"></div>
-                        <div class="white-bg"></div>
+                            <div className="gradient-bg">
+                                <div className="contact-info">
+                                    <div className="email-info">
+                                        <div className="icon">
+                                        </div>
+                                        <h1>info@pinnaclestudios.co</h1>
+                                    </div>
+                                    <div className="phone-info">
+                                        <div className="icon"></div>
+                                        <h1>+1 (408) 384 8602</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="white-bg"></div>
                             <div className="form-wrapper">
+                                <h1 className='contact-us-form-heading'>Let's book a free 30 minutes consultation to learn more about your project</h1>
                                 <form className="form">
-                                    <input type="text" name="name" id="name" />
+                                    <div className="top-part">
+                                        <input type="text" name="name" id="name" placeholder="Name" required/>
+                                        <input type="text" name="email" id="email"  placeholder="Email" required/>
+                                    </div>
+                                    <div className="bot-part">
+                                        <input type="text" name="company" id="company" placeholder="Company" required/>
+                                        <input type="text" name='number' id='number' placeholder='Number' required/>
+                                    </div>
+                                    <textarea name="message" id='Message' placeholder='Message' required></textarea> 
+                                    <button type="button">Book Free Consultation</button> 
                                 </form>
                             </div>
                         </div>
