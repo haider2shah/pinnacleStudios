edb2b938b4e230735d516d031801286ceb6ba02a
hamza <hamza.h96343@gmail.com>
2023-06-06 00:07:11 -0700

    path

--- pinnacle-studios/server.js
+++ pinnacle-studios/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   );
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
 }
 
