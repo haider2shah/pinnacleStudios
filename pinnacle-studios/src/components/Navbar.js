8449c84df464c359fde010e58dca09d8586d7921
hamza <hamza.h96343@gmail.com>
2023-06-30 19:55:54 -0700

    useEffectFix

--- pinnacle-studios/src/components/Navbar.js
+++ pinnacle-studios/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Nbg} from '../assets/navBackground.svg';
