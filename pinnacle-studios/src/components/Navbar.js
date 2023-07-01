323dd895d4b0617b7e1ae644881f3d223d366309
hamza <hamza.h96343@gmail.com>
2023-06-30 20:38:07 -0700

    MResFixesButtons

--- pinnacle-studios/src/components/Navbar.js
+++ pinnacle-studios/src/components/Navbar.js
@@ -1,96 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Nbg} from '../assets/navBackground.svg';
 import { ReactComponent as X } from '../assets/xButton.svg';
+import { Swipeable } from 'react-swipeable';
 
 const Navbar = ({ className }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [initialTouchPos, setInitialTouchPos] = useState(null);
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    const handleTouchMove = (event) => {
-      if (isOpen) {
-        event.preventDefault();
-      }
-    };
-
-    const handleBodyScroll = () => {
-      if (isOpen) {
-        document.body.style.overflow = 'hidden';
-      } else {
-        document.body.style.overflow = '';
-      }
-    };
-
-    document.addEventListener('touchmove', handleTouchMove, {
-      passive: false,
-    });
-    document.addEventListener('scroll', handleBodyScroll);
-
-    return () => {
-      document.removeEventListener('touchmove', handleTouchMove);
-      document.removeEventListener('scroll', handleBodyScroll);
-    };
-  }, [isOpen]);
+  const handleSwipeUp = () => {
+    if(isOpen){
+      setIsOpen(false)
+    }
+  }
 
   return (
-    <nav className="navbar">
-      <div className={`navbar-width ${className}`}>
-        <div className="logo"><Link to="/">PINNACLE STUDIOS</Link></div>
-        <div className={`${isOpen ? 'openNav' : 'contactPlusNav'}`}>
-          <div className='button-container'>
+    <Swipeable onSwipedUp={handleSwipeUp}>
+      <nav className="navbar">
+        <div className={`navbar-width ${className}`}>
+          <div className="logo"><Link to="/">PINNACLE STUDIOS</Link></div>
+          <div className={`${isOpen ? 'openNav' : 'contactPlusNav'}`}>
+            <div className='button-container'>
+              
+              <Link to="/contactuspage">
+                <button className='contact-us'>
+                  <h1>Contact Now</h1>
+                </button>
+              </Link>
+            </div>
+            <button className='navbar-toggle' 
+            onClick={toggleNavbar}>
+                <span className="hamburger"></span>
+                <span className="hamburger"></span>
+                <span className="hamburger"></span>
+            </button>
             
-            <Link to="/contactuspage">
-              <button className='contact-us'>
-                <h1>Contact Now</h1>
-              </button>
-            </Link>
           </div>
-          <button className='navbar-toggle' 
-          onClick={toggleNavbar}>
-              <span className="hamburger"></span>
-              <span className="hamburger"></span>
-              <span className="hamburger"></span>
-          </button>
+          <div className={`${isOpen ? 'navbar-show' : 'navbar-hide'}`}>
+            <div className="left">
+            </div>
+            <div className="border">
+              <Nbg className='background'/>
+            </div>
+            <div className="right">
+              <ul >
+                <div className="closePlusLink">
+                  <li><Link to="/" className="navbar-link">
+                    Home
+                  </Link></li>
+                  <div className="x-show" onClick={toggleNavbar}>
+                    <X className={`${(isOpen) ? 'xSvg' : 'xSVGhidden'}`}/>
+                  </div>
+                </div>
+                <li><Link to="/aboutuspage" className="navbar-link">
+                  About us
+                </Link></li>
+                <li><Link to="/servicespage" className="navbar-link">
+                  Services
+                </Link></li>
+                <li><Link to="/contactuspage" className="navbar-link">
+                    Contact
+                </Link></li>
+              </ul>
+              <h1 className='navBottomRightHeading'>PINNACLE STUDIOS</h1>
+            </div>
+          </div>
           
         </div>
-        <div className={`${isOpen ? 'navbar-show' : 'navbar-hide'}`}>
-          <div className="left">
-          </div>
-          <div className="border">
-            <Nbg className='background'/>
-          </div>
-          <div className="right">
-            <ul >
-              <div className="closePlusLink">
-                <li><Link to="/" className="navbar-link">
-                  Home
-                </Link></li>
-                <div className="x-show" onClick={toggleNavbar}>
-                  <X className={`${(isOpen) ? 'xSvg' : 'xSVGhidden'}`}/>
-                </div>
-              </div>
-              <li><Link to="/aboutuspage" className="navbar-link">
-                About us
-              </Link></li>
-              <li><Link to="/servicespage" className="navbar-link">
-                Services
-              </Link></li>
-              <li><Link to="/contactuspage" className="navbar-link">
-                  Contact
-              </Link></li>
-            </ul>
-            <h1 className='navBottomRightHeading'>PINNACLE STUDIOS</h1>
-          </div>
-        </div>
-        
-      </div>
-    </nav>
+      </nav>
+    </Swipeable>
   );
 };
 
